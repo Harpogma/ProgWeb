fb9c8763a0415244d5fc76e9837d31e08d49ea69
Harpogma <gd.temp@pm.me>
2025-03-21 09:32:05 +0100

    first commit

---
+++ Challenge_1/javaVersJS.js
@@ -0,0 +1,6 @@
+console.log("1er mouton")
+let i = 2
+while (i <= 23) {
+    console.log(i + "e mouton")
+    i += 1
+}
---
+++ Challenge_1/phpVersJS.js
@@ -0,0 +1,5 @@
+function calculerSurface(rayon) {
+    return (Math.PI * Math.pow(rayon, 2));
+}
+
+console.log(calculerSurface(4))
