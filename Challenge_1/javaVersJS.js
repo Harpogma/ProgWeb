d500fe64108b2c5cc782268894dd20df751b5e38
Harpogma <gd.temp@pm.me>
2025-09-16 08:32:58 +0200

    restart fresh for the new class

--- Challenge_1/javaVersJS.js
+++
@@ -1,6 +0,0 @@
-console.log("1er mouton")
-let i = 2
-while (i <= 23) {
-    console.log(i + "e mouton")
-    i += 1
-}
--- Challenge_1/phpVersJS.js
+++
@@ -1,5 +0,0 @@
-function calculerSurface(rayon) {
-    return (Math.PI * Math.pow(rayon, 2));
-}
-
-console.log(calculerSurface(4))
