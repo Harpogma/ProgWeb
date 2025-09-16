390e5371295e4d1957c8843496f8bb263f55aa75
Harpogma <gd.temp@pm.me>
2025-09-16 10:59:05 +0200

    Minor refactor

--- Challenge_1/script.js
+++ Challenge_1/script.js
@@ -1,19 +1,21 @@
 // Java vs JS
-
+console.log("Java VS JS");
 console.log("1er mouton");
 let i = 2;
 while (i <= 23) {
-    console.log(i + "e mouton");
-    i++;
+  console.log(i + "e mouton");
+  i++;
 }
 
 // PHP vs JS
+console.log("PHP VS JS");
 function calculerSurface(rayon) {
-    return (Math.PI * Math.pow(rayon, 2));
+  return (Math.PI * Math.pow(rayon, 2)).toFixed(2);
 }
 console.log(calculerSurface(4));
 
 // Code perso
+console.log("Code perso");
 for(let i = 1; i <= 100; i++) {
     if (i % 3 == 0 && i % 5 == 0) {
         console.log("Fizz buzz");
