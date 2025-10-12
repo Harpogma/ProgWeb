fbd445d75d94ea9bb70c657877259bdf0890e89e
Harpogma <gd.temp@pm.me>
2025-10-12 12:30:13 +0200

    Minor refactor

--- Challenge_5/script.js
+++ Challenge_5/script.js
@@ -1,9 +1,11 @@
 "use strict"
 
-// Tableau de nombres
+// Part 1
+
 const numbers = Object.freeze([3, 14, 15, 92 ,65, 35, 89, 79, 32, 38]);
 
 // 1
+console.log("Part 1");
 console.log(1)
 console.log(numbers);
 
@@ -90,3 +92,87 @@ console.log(12);
 const newNumbers3 = [...numbers];
 const sortBigToSmall = newNumbers3.sort((a,b) => b - a);
 console.log(sortBigToSmall);
+
+
+// Part 2
+console.log("Part 2");
+
+const strings = Object.freeze(["Sator", "Arepo", "Tenet", "Opera", "Rotas"]);
+
+// 1
+console.log(1);
+const wordWithR = () => {
+    let tempArr = [];
+    for (let word of strings) {
+        if (word.includes("r")) {
+            tempArr.push(word);
+        }
+    }
+    return tempArr;
+}
+console.log(wordWithR());
+
+// 2
+console.log(2);
+const wordOf5Letters = () => {
+    let tempArr = [];
+    for (let word of strings) {
+        if (word.length >= 5) {
+            tempArr.push(word);
+        }
+    }
+    return tempArr;
+}
+console.log(wordOf5Letters());
+
+// 3 
+console.log(3);
+const stringsCopy = [...strings];
+console.log(stringsCopy);
+const arrWithNewWordFront = (word) => [word].concat(stringsCopy);
+console.log(arrWithNewWordFront("New word"));
+
+// 4
+console.log(4);
+const stringsCopy2 = [...strings]; 
+const arrWithNewWordEnd = (word) => stringsCopy2.concat(word);
+console.log(arrWithNewWordEnd("New word")); 
+
+// 5
+console.log(5);
+const stringsCopy3 = [...strings];
+const replaceMiddleWord = (word) => {
+    let middleIndex = 0;
+    if (stringsCopy3.length % 2 === 0) {
+        middleIndex = (stringsCopy3.length / 2) - 1;
+    } else {
+        middleIndex = Math.floor(stringsCopy3.length / 2);
+    }
+
+    stringsCopy3[middleIndex] = word;
+    return stringsCopy3;
+}
+console.log(replaceMiddleWord("New Word"));
+
+// 6
+console.log(6);
+console.log(strings.join())
+
+// 7
+console.log(7);
+const firstWordOfArr = () => {
+    let firstWord = strings[0];
+    for(let word of strings) {
+        if (firstWord.localeCompare(word) > 0) {
+            firstWord = word;
+        }
+    }
+    return firstWord;
+}
+console.log(firstWordOfArr());
+
+// 8y
+console.log(8);
+const isPalindrome = () => {
+    
+}
